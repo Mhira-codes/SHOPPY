6d4edcd0b3be886bd3694c034d45abbaaf16c751
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-31 17:57:58 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from './component/Header/Header';
+import Hero from './component/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header/>
+    <Hero/>
     </div>
   );
 }
