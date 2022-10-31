08a0c6acb04d52d23d113e0829f40fd54b44609d
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-31 18:03:52 +0000

    file update

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/logo.png";
+import { CgShoppingBag } from "react-icons/cg";
+
+const Header = () => {
+  return (
+    <div className="container flex justify-between items-center p-2 mt-5">
+      <div className="logo flex items-center gap-2 ">
+        <img src={logo} alt="" className="w-[2.5rem] h-[2.5rem]" />
+        <span className="text-base font-semibold">shoppy</span>
+      </div>
+      <div className="right-side flex items-center justify-center  gap-2">
+        <div className="nav-list ">
+          <ul className="flex justify-center items-center gap-3 list-none font-medium text-[16px] cursor-pointer">
+            <li>Collection</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>ENG</li>
+       
+          </ul>
+        </div>
+        <div>
+        <input type="text" placeholder="search" className="search  w-full  border-collapse outline-none px-1 rounded"  />
+        </div>
+      <div >
+      <CgShoppingBag  className="w-full h-6 cursor-pointer "/>
+      </div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Header;
