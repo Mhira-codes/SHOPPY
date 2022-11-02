228bcbb819a2d3c58fe1120854015a501e69df40
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-02 18:02:59 +0000

    update

--- src/component/Products/Product.jsx
+++ src/component/Products/Product.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 const Product = () => {
   const [productItems, setProductItems] = useState(ProductsData);
   return (
-    <div className="flex flex-col gap-8 items-center justify-center   pb-[2rem] pl-[2rem] relative">
+    <div className="flex flex-col gap-8 items-center    pb-[2rem] pl-[2rem] relative">
       <div className="product header flex flex-col justify-start  items-start ">
         <img
           src={headerImg}
@@ -18,8 +18,8 @@ const Product = () => {
         </h1>
       </div>
 
-      <div className="porduct wrapper">
-        <ul>
+      <div className="product-wrapper w-[90%]">
+        <ul className="text-[1.3rem] flex flex-col gap-8 font-[500] cursor-pointer">
           <li>All</li>
           <li>Skin Care </li>
           <li>Conditioners</li>
@@ -28,7 +28,20 @@ const Product = () => {
 
         <div className="product-list">
           {productItems.map((item, i) => (
-            <div className="product">effjwiad</div>
+            <div className="product w-[12rem] h-[9rem] bg-white relative overflow-hidden  rounded-2xl flex gap-[1rem]
+             p-[1rem]">
+              <div className="left-side flex flex-col justify-between">
+                <div className="title flex flex-col">
+                  <span className="text-[1.2rem] font-[600]">{item.name}</span>
+                  <span className="text-[0.7rem]">{item.detail}</span>
+                </div>
+                <span className="text-[1.5rem] font-[600]">{item.price}$</span>
+                <div className="btn text-[0.6rem] border border-solid border-[#343434]  rounded-2xl ">
+                  shop now
+                </div>
+              </div>
+              <img src={item.img} alt="" className="w-[6rem] h-[11rem]  -rotate-[20deg] right-0 "/>
+            </div>
           ))}
         </div>
       </div>
