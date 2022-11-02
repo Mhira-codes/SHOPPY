b5e4a445fcdc42cced9658673ab70a8381282e4a
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-02 09:00:47 +0000

    added components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react'
 import './App.css';
 import Header from './component/Header/Header';
 import Hero from './component/Hero/Hero';
+import Slider from './component/Slider/Slider';
 
 function App() {
   return (
     <div className="App">
     <Header/>
-    <Hero/>
+    <div className='mt-7'>
+    <Hero />
+    </div>
+<div>
+<Slider/>
+</div>
+    
     </div>
   );
 }
