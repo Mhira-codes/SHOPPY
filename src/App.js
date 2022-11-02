7a055d1a2ce8d40d859995137ec69e9030f6d68d
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-02 16:42:57 +0000

    file updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import './App.css';
-import Header from './component/Header/Header';
-import Hero from './component/Hero/Hero';
-import Slider from './component/Slider/Slider';
+import React from "react";
+import "./App.css";
+import Header from "./component/Header/Header";
+import Hero from "./component/Hero/Hero";
+import Product from "./component/Products/Product";
+import Slider from "./component/Slider/Slider";
+import VIrtual from "./component/Virtual/VIrtual";
 
 function App() {
   return (
     <div className="App">
-    <Header/>
-    <div className='mt-7'>
-    <Hero />
-    </div>
-<div>
-<Slider/>
-</div>
-    
+      <Header />
+      <div className="mt-7">
+        <Hero />
+      </div>
+      <div>
+        <Slider />
+      </div>
+      <div>
+        <VIrtual />
+      </div>
+      <div>
+        <Product/>
+      </div>
     </div>
   );
 }
