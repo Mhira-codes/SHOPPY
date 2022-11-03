ade3e88b3d3fc8f65ef7e995632ab7f533d6a3ac
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-03 18:13:32 +0000

    file updated

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -3,7 +3,10 @@ import "./Hero.css";
 import heroImg from "../../assets/hero.png";
 import { RiShoppingBagFill } from "react-icons/ri";
 import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion"
 const Hero = () => {
+
+  const transition ={duration:3, type:"spring"}
   return (
     <div className="hero-section">
       {/* left-side */}
@@ -19,9 +22,21 @@ const Hero = () => {
       </div>
       {/* middle-side */}
       <div className="middle-side flex items-baseline justify-center relative">
-        <div className="blue-img w-[30rem] h-[30rem] absolute -z-[99] rounded-full bottom-0"></div>
-        <img src={heroImg} alt="" width={600} className="w-[30rem] absolute bottom-0" />
-        <div className="cart absolute bottom-[23%] right-[4%] flex gap-[1rem]">
+        <motion.div 
+        initial={{bottom:'2rem'}}
+        whileInView={{bottom:'0rem'}}
+        transition={transition}
+        className="blue-img w-[30rem] h-[30rem] absolute -z-[99] rounded-full bottom-0"></motion.div>
+        <motion.img 
+        initial={{bottom:"-2rem"}}
+        whileInView={{bottom:"0rem"}}
+        transition={transition}
+        src={heroImg} alt="" width={600} className="w-[30rem] absolute bottom-0" />
+        <motion.div 
+        initial={{right:'2%'}}
+        whileInView={{right:'4%'}}
+        transition={transition}
+        className="cart absolute bottom-[23%] right-[4%] flex gap-[1rem]">
             <div>
             <RiShoppingBagFill className="w-[60px] h-[60px] bg-white   border-[6px] border-gray-800 border-solid rounded-full p-[10px]"/>
             </div>
@@ -32,7 +47,7 @@ const Hero = () => {
               <BsArrowRight className="arrow w-[10px] h-[10px] " />
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
       {/* right-hand */}
       <div className="hero-sides flex flex-col text-right">
