5290ad678d605eff11233585018ba4d2d4e4dd98
mhira-codes <alhassansamiratu991@gmail.com>
2022-11-03 18:15:05 +0000

    rate.jsx file added

---
+++ src/component/Rate/Rate.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Rate.css";
+import rateImg from "../../assets/testimonialHero.png";
+const Rate = () => {
+  return (
+    <div className="rate  flex justify-center items-center flex-col">
+      <div className="rate-wrapper grid  w-full">
+        {/* left */}
+        <div className="flex flex-col text-left gap-3">
+          <span className="text-3xl uppercase font-bold">Top rated</span>
+          <p className="text-[0.9rem]">
+            SEEDILY SAY HAS SUITABLE DISPOSAL AND BOY. EXERCISE JOY MAN CHILDREN
+            REJOICED
+          </p>
+        </div>
+
+        {/* middle */}
+        <div className="flex  justify-center">
+        <img src={rateImg} alt="" className="w-[25rem]   " />
+        </div>
+        
+      
+
+        {/* rigt-side */}
+        <div className="flex flex-col text-right gap-3">
+          <span className="text-3xl uppercase font-bold">100k</span>
+          <span className="text-[0.9rem] uppercase">Happy customers with us</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rate;
